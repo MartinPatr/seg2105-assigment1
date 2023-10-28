f34667d113192f836b494c7e5df65307424adc40
MartinPatr <martinpatrouchev@gmail.com>
2023-10-28 18:24:25 -0400

    fixed up project to compile and etc

--- simpleChat/edu/seg2105/client/backend/ChatClient.java
+++ simpleChat/edu/seg2105/client/backend/ChatClient.java
@@ -18,92 +18,76 @@ import edu.seg2105.client.common.*;
  * @author Dr Robert Lagani&egrave;
  * @author Fran&ccedil;ois B&eacute;langer
  */
-public class ChatClient extends AbstractClient
-{
-  //Instance variables **********************************************
-  
+public class ChatClient extends AbstractClient {
+  // Instance variables **********************************************
+
   /**
-   * The interface type variable.  It allows the implementation of 
+   * The interface type variable. It allows the implementation of
    * the display method in the client.
    */
-  ChatIF clientUI; 
+  ChatIF clientUI;
 
-  
-  //Constructors ****************************************************
-  
+  // Constructors ****************************************************
+
   /**
    * Constructs an instance of the chat client.
    *
-   * @param host The server to connect to.
-   * @param port The port number to connect on.
+   * @param host     The server to connect to.
+   * @param port     The port number to connect on.
    * @param clientUI The interface type variable.
    */
-  
-  public ChatClient(String host, int port, ChatIF clientUI) 
-    throws IOException 
-  {
-    super(host, port); //Call the superclass constructor
+
+  public ChatClient(String host, int port, ChatIF clientUI)
+      throws IOException {
+    super(host, port); // Call the superclass constructor
     this.clientUI = clientUI;
     openConnection();
   }
 
-  
-  //Instance methods ************************************************
-    
+  // Instance methods ************************************************
+
   /**
    * This method handles all data that comes in from the server.
    *
    * @param msg The message from the server.
    */
-  public void handleMessageFromServer(Object msg) 
-  {
+  public void handleMessageFromServer(Object msg) {
     clientUI.display(msg.toString());
+
   }
 
   /**
-   * This method handles all data coming from the UI            
+   * This method handles all data coming from the UI
    *
-   * @param message The message from the UI.    
+   * @param message The message from the UI.
    */
-  public void handleMessageFromClientUI(String message)
-  {
-    try
-    {
+  public void handleMessageFromClientUI(String message) {
+    try {
       sendToServer(message);
-    }
-    catch(IOException e)
-    {
-      clientUI.display
-        ("Could not send message to server.  Terminating client.");
+    } catch (IOException e) {
+      clientUI.display("Could not send message to server.  Terminating client.");
       quit();
     }
   }
-  
+
   /**
    * This method terminates the client.
    */
-  public void quit()
-  {
-    try
-    {
+  public void quit() {
+    try {
       closeConnection();
+    } catch (IOException e) {
     }
-    catch(IOException e) {}
     System.exit(0);
   }
 
-  @Override
   protected void connectionException(Exception exception) {
-    System.out.println("The server has shut down.");
+    clientUI.display("The server is shut down");
     quit();
   }
 
-  @Override
   protected void connectionClosed() {
-    System.out.println("The connection has been closed.");
+    clientUI.display("Connection is closed");
   }
-
-
-
 }
-//End of ChatClient class
+// End of ChatClient class
